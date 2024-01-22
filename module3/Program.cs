6f4b1b8733e530f12f85a1bdac63843e665f9aef
Nik Nazarov <nike242@mail.ru>
2024-01-22 18:54:30 +0300

    first commit

---
+++ module3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace module3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            myName = "Nik";
+
+            Console.WriteLine(myName);
+
+            Console.ReadKey();
+        }
+    }
+}
