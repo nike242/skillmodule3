deae9ebd978db95517279ba998aade19429dd3bf
Nik Nazarov <nike242@mail.ru>
2024-01-22 19:17:00 +0300

    end task1

--- module3/Program.cs
+++ module3/Program.cs
@@ -1,14 +1,20 @@
-﻿namespace module3
+﻿using System.Xml.Linq;
+
+namespace module3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string myName;
-            myName = "Nik";
 
-            Console.WriteLine(myName);
+            const string MyName = "Nik"
+            Console.WriteLine(MyName);
 
+            Console.WriteLine("\t Привет Мир");
+            Console.WriteLine("\t Мне 27 лет");
+            Console.WriteLine("\t My name is \n {0}", MyName);
+            Console.WriteLine("\u0040");
+            Console.WriteLine("\u0023");
             Console.ReadKey();
         }
     }
